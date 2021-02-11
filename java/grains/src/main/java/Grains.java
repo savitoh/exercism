878546b6a0c4450f75b1b16ee5ae19e237989052
Sávio Raires <savioraires10@gmail.com>
2021-02-10 21:52:37 -0300

    Excercicio grains: Removendo números mágicos

--- java/grains/src/main/java/Grains.java
+++ java/grains/src/main/java/Grains.java
@@ -4,21 +4,25 @@ import java.util.stream.IntStream;
 
 class Grains {
 
+    private static final int MAX_NUMBER_FIELDS = 64;
+
+    private static final int MIN_NUMBER_FIELDS = 1;
+
     private final BinaryOperator<BigInteger> progressionFunction =
             (subtotal, element) -> subtotal.equals(BigInteger.ZERO)? BigInteger.ONE : subtotal.multiply(BigInteger.valueOf(2));
 
     BigInteger grainsOnSquare(final int square) {
-        if(square <= 0 || square > 64) {
+        if(square < MIN_NUMBER_FIELDS || square > MAX_NUMBER_FIELDS) {
             throw new IllegalArgumentException("square must be between 1 and 64");
         }
-        return IntStream.rangeClosed(1, square)
+        return IntStream.rangeClosed(MIN_NUMBER_FIELDS, square)
                 .mapToObj(BigInteger::valueOf)
                 .reduce(progressionFunction)
                 .orElseThrow(RuntimeException::new);
     }
 
     BigInteger grainsOnBoard() {
-        return IntStream.rangeClosed(1, 64)
+        return IntStream.rangeClosed(MIN_NUMBER_FIELDS, MAX_NUMBER_FIELDS)
                 .mapToObj(this::grainsOnSquare)
                 .reduce(BigInteger::add)
                 .get();
